3997ad7fb7e79ca2a905a523c7dc8811c0caf0f1
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-15 04:49:37 +0300

    Refactored Code.

--- gomoku.c
+++ gomoku.c
@@ -1,6 +1,7 @@
 // Headers //
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Definements //
 #define CLEAR_BUFFER while (getchar() != '\n');
@@ -24,63 +25,57 @@ int play_again();
 int main(int agrc, char * argv[]) {
     // Variables  //
     int play, size, turn, row, col, result;
-    int player_wins, draws, computer_wins;
+    int player_wins = 0, draws = 0, computer_wins = 0;
     char board[BOARD_SIZE][BOARD_SIZE];
-
     size = BOARD_SIZE;
-    play = 1;
-    player_wins = 0;
-    draws = 0;
-    computer_wins = 0;
+    play = 1; // To enter the while loop.
+
+    // Seed the random number generator //
+    srand(time(NULL));
 
     // Game Loop //
     while(play) {
-        printf("-- G O M O K U ---\n");
+        printf("--- G O M O K U ---\n");
 
         init_board(board, size);
         print_stats(player_wins, draws, computer_wins);
         print_board(board, size);
 
         turn = 1;
-        while(winner(board, size, turn, &result) == 0) { //    
+        while(winner(board, size, turn, &result) == 0) {
             if (next_turn(turn) == 1) {
                 player(board, size, &row, &col);
                 board[row][col] = PLAYER;
-            }
-            else if (next_turn(turn) == 0) {
+            } else {
                 computer(board, size, &row, &col);
                 board[row][col] = COMPUTER;            
             }
-            
             print_stats(player_wins, draws, computer_wins);
             print_board(board, size);
             turn++;
         }
         update_stats(result, &player_wins, &draws, &computer_wins);
         print_result(result);
-
         play = play_again();
     }
     return 0;
 }
 
-// Functions //
+// Functions Implementation //
 void init_board(char board[BOARD_SIZE][BOARD_SIZE], int size) {
-    int i, j;
-
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) { board[i][j] = '+'; }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) { 
+            board[i][j] = '+'; 
+        }
     }
 }
 
 void print_board(char board[BOARD_SIZE][BOARD_SIZE], int size) {
-    int i,j;
-
     printf("----------------------------------------------------------------\n");
-    for(i = 0; i < size; i++) {
+    for(int i = 0; i < size; i++) {
         printf("|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n");
         printf("|---");
-        for ( j = 0; j < size; j++ ) { printf("%c---", board[i][j]); }
+        for (int j = 0; j < size; j++ ) { printf("%c---", board[i][j]); }
         printf("|\n");
     }
     printf("|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n");
@@ -90,7 +85,7 @@ void print_board(char board[BOARD_SIZE][BOARD_SIZE], int size) {
 void print_stats(int player_wins, int draws, int computer_wins) {
     printf("Current Sessions results!\n"
             "-------------------------\n"
-            "Your wins: %d\n"
+            "Player Wins: %d\n"
             "Draws: %d\n"
             "Computer Wins: %d\n"
             "-------------------------\n",
@@ -98,25 +93,32 @@ void print_stats(int player_wins, int draws, int computer_wins) {
 }
 
 void update_stats(int result, int *player_wins, int *draws, int *computer_wins) {  
-    if (result == 1) { *player_wins += 1; }
-    else if (result == 2) { *computer_wins += 1; }
-    else if (result == -1) { *draws += 1; }
+    if (result == 1) { 
+        *player_wins += 1; 
+    } else if (result == 2) { 
+        *computer_wins += 1; 
+    } else if (result == -1) { 
+        *draws += 1; 
+    }
 }
 
 void print_result(int result) {    
-    if (result == 1) { printf("Congratulations! You win!\n"); }
-    else if (result == 2) { printf("Computer takes this one.\n"); }
-    else if (result == -1) { printf("No winner. Its a draw!\n"); }
+    if (result == 1) { 
+        printf("Congratulations! You win!\n"); 
+    } else if (result == 2) { 
+        printf("Computer takes this one.\n"); 
+    } else if (result == -1) { 
+        printf("No winner. Its a draw!\n"); 
+    }
 }
 
 int next_turn(int turn) {
-    turn = turn % 2; // Either 0 or 1.
-    return turn;
+    return (turn % 2) + 1;
 }
 
 int winner(char board[BOARD_SIZE][BOARD_SIZE], int size, int turn, int *result) {
     // Variables //
-    int con_row, con_col, i, j, found;
+    int con_row, con_col, found;
     int spaces = 225;
     found = 0;
 
@@ -124,113 +126,112 @@ int winner(char board[BOARD_SIZE][BOARD_SIZE], int size, int turn, int *result) 
         con_row = 0;
         con_col = 0;
 
-        for (i=0; i<size; i++) {
-            for (j=0; j<size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (board[i][j] == PLAYER) {
-                    con_row += 1;
-                    if (con_row == CHAIN) { found = 1; }
+                    con_row++;
+                    if (con_row == CHAIN) { 
+                        found = 1; 
+                    }
+                } else { 
+                    con_row = 0; 
                 }
-                else { con_row = 0; }
             }
         }
-        for (j=0; j<size; j++) {
-            for (i=0; i<size; i++) {
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
                 if (board[i][j] == PLAYER) {
-                    con_col += 1;
-                    if (con_col == CHAIN) { found = 1; }
-                    else { con_col = 0; }
+                    con_col++;
+                    if (con_col == CHAIN) { 
+                        found = 1; 
+                    } else { 
+                        con_col = 0; 
+                    }
                 }
             }
         }
     }
     
     spaces -= 1;
-
     if (spaces != 0) { // Computer.
         if ((turn % 2) == 0) {
             con_row = 0;
             con_col = 0;
 
-            for (i=0; i<size; i++) {
-                for ( j= 0; j<size; j++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
                     if (board[i][j] == COMPUTER) {
-                        con_row += 1;
-                        if (con_row == CHAIN) { found = 1; }
-                    }
-                    else { con_row = 0; }
-                }
-            }
-            for (j=0; j<size; j++){
-                for (i=0; i<size; i++) {
-                    if (board[i][j] == COMPUTER) {
-                        con_col += 1;
-                        if (con_col == CHAIN) { found = 1; }
-                        else { con_col = 0; }
+                        con_row++;
+                        if (con_row == CHAIN) { 
+                            found = 1; 
+                        }
+                    } else { 
+                        con_row = 0; 
                     }
                 }
             }
-            spaces -= 1;
+            for (int j = 0; j < size; j++){
+                for (int i = 0; i < size; i++) {
+                    if (board[i][j] == COMPUTER) {
+                        con_col++;
+                        if (con_col == CHAIN) { 
+                            found = 1; 
+                        } else { 
+                            con_col = 0; 
+                        }
+                    }
+                }
+            }
+            spaces--;
         }
     }
 
-    if (found == 1) // We have a winner.
-    {
-        if ((turn % 2) == 1) {*result = 1; } // User.
-        else if ((turn % 2) == 0) { *result = 2; } // Computer.
+    if (found == 1) { // Winner.
+        if ((turn % 2) == 1) { // Player.
+            *result = 1; 
+        } else { // Computer.
+            *result = 2; 
+        }
     }
     else if (found == 0) { // Draw.
-        if (spaces == 0) { *result = -1; }
-        else {*result = 0;} // Continue.
+        if (spaces == 0) { 
+            *result = -1; 
+        } else { // Continue.
+            *result = 0;
+        }
     }
-
     return *result;
 }
 
 void player(char board[BOARD_SIZE][BOARD_SIZE], int size, int *row, int *col) {
-    // Variables //
-    int fun_row, fun_col;
-
     do {
         do { // Row.
             printf("Give the Row you want to place the X: ");
-            scanf("%d", &fun_row);
-        } while (fun_row<0 || fun_row>BOARD_SIZE);
+            scanf("%d", row);
+        } while (*row < 1 || *row > size);
 
         do { // Column.
             printf("Give the Column you want to place the X: ");
-            scanf("%d", &fun_col);
-        } while (fun_col<0 || fun_col>BOARD_SIZE);
+            scanf("%d", &col);
+        } while (*col < 1 || *col > size);
 
-    } while (board[fun_row-1][fun_col-1] != '+');
-
-    *col = fun_col - 1;
-    *row = fun_row - 1;
+        (*row)--;
+        (*col)--;
+    } while (board[*row][*col] != '+');
 }
 
 void computer(char board[BOARD_SIZE][BOARD_SIZE], int size, int *row, int *col) {
-    // Variables //
-    int fun_row, fun_col;
-
     do {
-        fun_row = rand() % 16;
-        fun_col = rand() % 16;
+        *row = rand() % size;
+        *col = rand() % size;
 
-    } while (board[fun_row-1][fun_col-1] != '+');
-
-    *row = fun_row - 1;
-    *col = fun_col - 1;
+    } while (board[*row][*col] != '+');
 }
 
 int play_again() {
-    // Variables //
     char question;
-    int play;
-
     printf("Do you want to play again? (y/n)\n--> ");
     scanf("%c", &question);
-
-    if (question == 'y') { play = 1; }
-    else { play = 0; }
-
-    return play;
+    CLEAR_BUFFER;
+    return (question == 'y' || question == 'Y'); // Casse sensitive.
 }
